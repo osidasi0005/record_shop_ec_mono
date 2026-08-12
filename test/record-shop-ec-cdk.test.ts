1d397017c005c4188cff05785c32c8c4aeb5047d
osidasi0005 <osidasi0005@gmail.com>
2026-08-12 20:48:27 +0900

    Initial commit

---
+++ test/record-shop-ec-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecordShopEcCdk from '../lib/record-shop-ec-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/record-shop-ec-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecordShopEcCdk.RecordShopEcCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
